72d3296c7897cf0fdc0c45dc747e2872f2ea16ed
magervalp <magervalp>
2003-05-07 17:02:01 +0000

    exporting vectors

--- vectors.s
+++ vectors.s
@@ -1,3 +1,7 @@
+	.export irqvector
+	.export resetvector
+	.export nmivector
+
 	.import reseth
 
 	.code
@@ -7,6 +11,9 @@ nmih:	rti
 	
 	.segment "VECTORS"
 
+irqvector:
 	.addr irqh
+resetvector:
 	.addr reseth
+nmivector:
 	.addr nmih
