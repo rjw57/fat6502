3c50045b7ce3562bc88e37d472a496d321a3eb51
magervalp <magervalp>
2003-09-19 21:00:23 +0000

    added boot from rom bank 5, with crc check

--- alternaterom.s
+++ alternaterom.s
@@ -7,10 +7,9 @@
 	.import updatecrc
 	.importzp crc
 
-	.import debug_init
-	.import debug_puts
-	.import debug_puthex
-	.import debug_crlf
+	.importzp ptr
+
+	.import warmstartvector
 
 
 	.segment "RELOC"
@@ -18,33 +17,18 @@
 orgcrc:		.res 4
 ctr:		.res 1
 
-rombank		= 3
+rombank		= 2
 
 
 checkaltrom:
-	jsr debug_init		;;;;; remove me!
+	jsr setrombank5
 
-	bit flash_clear		; set flash address to $05c000
-	bit flash_inc		; 1 1100 0000 0000 0000
-	bit flash_shift
-	bit flash_inc
-	bit flash_shift
-	bit flash_inc
-	ldx #14
-:	bit flash_shift
-	dex
-	bne :-
-
-	ldax orgcrcmsg
-	jsr debug_puts
 	ldx #3			; save crc for comparison
 :	lda flash_data + rombank
 	sta orgcrc,x
-	jsr debug_puthex
 	bit flash_inc
 	dex
 	bpl :-
-	jsr debug_crlf
 
 	jsr makecrctables	; init crc32
 	jsr resetcrc
@@ -67,22 +51,53 @@ checkaltrom:
 	cpy #252
 	bne :-
 
-	ldax calccrcmsg
-	jsr debug_puts
 	ldx #3
 :	lda crc,x
 	eor #$ff
-	jsr debug_puthex
+	cmp orgcrc,x
+	bne fail
 	dex
 	bpl :-
-	jsr debug_crlf
 
-	jmp *
+	jsr setrombank5
+	ldax $c000
+	stax ptr
+	ldy #0
+	ldx #$40
+:	lda flash_data + rombank
+	sta (ptr),y
+	bit flash_inc
+	iny
+	bne :-
+	inc ptr + 1
+	dex
+	bne :-
+
+	sei
+	cld
+	ldx #$ff
+	txs
+
+	ldx #0			; clear zp and stack
+	txa
+:	sta $00,x
+	sta $0100,x
+	inx
+	bne :-
+
+	jmp (warmstartvector)
+
+
+setrombank5:
+	bit flash_clear		; set flash address to $05c000
+	bit flash_inc		; 1 1100 0000 0000 0000
+	bit flash_shift
+	bit flash_inc
+	bit flash_shift
+	bit flash_inc
+	ldx #14
+:	bit flash_shift
+	dex
+	bne :-
+fail:
 	rts
-
-
-orgcrcmsg:
-	.byte "Original CRC-32 checksum: ",0
-
-calccrcmsg:
-	.byte "Calculated CRC-32 checksum: ",0
